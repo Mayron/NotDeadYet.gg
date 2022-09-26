1f1c69f6dc1ee1c1b14eecb92a3394638a766d7c
Mayron <mjr_4077@hotmail.co.uk>
2022-09-26 22:21:23 +0100

    Updated results

--- components/forms/containers/your-application.tsx
+++ components/forms/containers/your-application.tsx
@@ -44,54 +44,151 @@ const YourApplication: React.FC<IYourApplicationProps> = ({ application }) => {
         <header>
           <h2>Requirements</h2>
         </header>
-        <div>
-          <StyledQuestion>
-            <p>
-              <StyledLabel>Question:</StyledLabel> Are you able to consistently raid 3
-              times per week for progression, and 2 times per a week for farm content
-              between 19:30-23:00 (server time)?
-            </p>
-            <p>
-              <StyledLabel>Answer:</StyledLabel>
-              {application.availability === "full"
-                ? "Yes"
-                : application.partialAvailabilityReason}
-            </p>
-          </StyledQuestion>
-        </div>
+
+        <StyledQuestion>
+          <p>
+            Are you able to consistently raid 3 times per week for progression, and 2
+            times per a week for farm content between 19:30-23:00 (server time)?
+          </p>
+          <p>
+            <StyledLabel>Answer:</StyledLabel>
+            {application.availability === "full"
+              ? "Yes"
+              : application.partialAvailabilityReason}
+          </p>
+        </StyledQuestion>
       </WhitePanel>
 
       <WhitePanel>
         <header>
           <h2>Character Information</h2>
         </header>
-        <div>
-          <div>characterName: {application.characterName}</div>
-          <div>characterClass: {application.characterClass}</div>
-          <div>characterMainSpec: {application.characterMainSpec}</div>
-          <div>primaryProfession1: {application.primaryProfession1}</div>
-          <div>primaryMaxLevel1: {application.primaryMaxLevel1}</div>
-          <div>primaryNotMaxedReason1: {application.primaryNotMaxedReason1}</div>
-          <div>primaryProfession2: {application.primaryProfession2}</div>
-          <div>primaryMaxLevel2: {application.primaryMaxLevel2}</div>
-          <div>primaryNotMaxedReason2: {application.primaryNotMaxedReason2}</div>
-        </div>
+
+        <StyledQuestion>
+          <StyledLabel>Character Name:</StyledLabel> {application.characterName}
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <StyledLabel>Class:</StyledLabel> {application.characterClass}
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <StyledLabel>Main Spec:</StyledLabel> {application.characterMainSpec}
+        </StyledQuestion>
+
+        <h3>Professions</h3>
+
+        <StyledQuestion>
+          <StyledLabel>Primary Profession #1:</StyledLabel>{" "}
+          {application.primaryProfession1}
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <p>
+            Is the skill level of this profession maxed out for the current expansion? If
+            not, why?
+          </p>
+          <p>
+            <StyledLabel>Answer:</StyledLabel>
+            {application.primaryMaxLevel1 === "yes"
+              ? "Yes"
+              : application.primaryNotMaxedReason1}
+          </p>
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <StyledLabel>Primary Profession #2:</StyledLabel>{" "}
+          {application.primaryProfession2}
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <p>
+            Is the skill level of this profession maxed out for the current expansion? If
+            not, why?
+          </p>
+          <p>
+            <StyledLabel>Answer:</StyledLabel>
+            {application.primaryMaxLevel2 === "yes"
+              ? "Yes"
+              : application.primaryNotMaxedReason2}
+          </p>
+        </StyledQuestion>
       </WhitePanel>
       <WhitePanel>
         <header>
           <h2>About You</h2>
         </header>
-        <div>
-          <div>canTalk: {application.canTalk}</div>
-          <div>cannotTalkReason: {application.cannotTalkReason}</div>
-          <div>bringConsumes: {application.bringConsumes}</div>
-          <div>cannotBringConsumesReason: {application.cannotBringConsumesReason}</div>
-          <div>wowExperience: {application.wowExperience}</div>
-          <div>describeSelf: {application.describeSelf}</div>
-          <div>discordId: {application.discordId}</div>
-          <div>vouch: {application.vouch}</div>
-          <div>anythingElse: {application.anythingElse}</div>
-        </div>
+
+        <StyledQuestion>
+          <p>
+            Are you able to use a microphone on discord and are happy to talk when needed?
+            If not, why?
+          </p>
+          <p>
+            <StyledLabel>Answer:</StyledLabel>
+            {application.canTalk === "yes" ? "Yes" : application.cannotTalkReason}
+          </p>
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <p>
+            Are you able to enough bring consumables (e.g., food, elixirs/flasks, weapon
+            enchants, etc...) for all boss attempts for each raid night per week? This
+            includes both progression and farm raid nights. If not, why?
+          </p>
+          <p>
+            <StyledLabel>Answer:</StyledLabel>
+            {application.bringConsumes === "yes"
+              ? "Yes"
+              : application.cannotBringConsumesReason}
+          </p>
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <p>
+            Please provide a short summary of your previous WoW experience, focusing
+            mainly on your raiding experience and achievements.
+          </p>
+          <p>
+            <StyledLabel>Answer:</StyledLabel>
+            {application.wowExperience}
+          </p>
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <p>
+            Briefly describe your approach/philosophy to raiding (e.g., are you
+            competitive, casual but effective, do you study boss tactics or world of
+            logs?).
+          </p>
+          <p>
+            <StyledLabel>Answer:</StyledLabel>
+            {application.describeSelf}
+          </p>
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <StyledLabel>Discord ID:</StyledLabel> {application.discordId}
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <p>(Optional) Do you know anyone in the guild that could vouch for you?</p>
+          <p>
+            <StyledLabel>Answer:</StyledLabel>
+            {application.vouch}
+          </p>
+        </StyledQuestion>
+
+        <StyledQuestion>
+          <p>
+            (Optional) Is there something we should know? Travelling, demanding family or
+            career that could get in the way of raiding etc.
+          </p>
+          <p>
+            <StyledLabel>Answer:</StyledLabel>
+            {application.anythingElse}
+          </p>
+        </StyledQuestion>
       </WhitePanel>
     </section>
   );
