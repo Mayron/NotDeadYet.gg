80feec8360dfa6465ff7fa1c098200a22187bf6c
Mayron <mjr_4077@hotmail.co.uk>
2022-10-13 11:48:31 +0100

    added everywhere

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     authorized: ({ token }) => {
       if (token?.admin) {
