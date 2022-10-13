262b0acd30319569e137ef9f4d53bad0f47a9655
Mayron <mjr_4077@hotmail.co.uk>
2022-10-13 14:57:36 +0100

    redirect to discord

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -1,3 +1,17 @@
+import Layout from "../components/layout";
+
+const DiscordRedirectPage: React.FC = () => (
+  <Layout title="Discord | Not Dead Yet">
+    <section>
+      <header>
+        <h1>Redirecting to Discord...</h1>
+      </header>
+    </section>
+  </Layout>
+);
+
+export default DiscordRedirectPage;
+
 export function getServerSideProps() {
   return {
     redirect: {
